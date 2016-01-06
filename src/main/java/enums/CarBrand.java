480f5402986c3b1013f6274d79999af2a7395db1
Harshita Kasera <hkasera5@gmail.com>
2016-01-06 16:54:07 +0530

    Added simple java models

---
+++ src/main/java/enums/CarBrand.java
@@ -0,0 +1,8 @@
+package enums;
+
+/**
+ * Created by harshita.k on 06/01/16.
+ */
+public enum CarBrand {
+    TATA,BMW,AUDI
+}
---
+++ src/main/java/models/Car.java
@@ -0,0 +1,22 @@
+package models;
+
+import enums.CarBrand;
+
+/**
+ * Created by harshita.k on 06/01/16.
+ */
+public class Car {
+    private CarBrand carBrand;
+
+    public Car(CarBrand carBrand) {
+        this.carBrand = carBrand;
+    }
+
+    public CarBrand getCarBrand() {
+        return carBrand;
+    }
+
+    public void setCarBrand(CarBrand carBrand) {
+        this.carBrand = carBrand;
+    }
+}
